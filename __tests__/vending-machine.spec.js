baf90d00fd13398716a822377c564ca8e3c0b485
Sophie Virtue <sophie.virtue@gmail.com>
2019-02-24 17:19:10 -0800

    print inventory test passed

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -1,93 +1,102 @@
-const vendingMachine = require('../src/vending-machine');
+const VendingMachine = require('../src/vending-machine');
 
-describe('vendingMachine', () => {
+describe('VendingMachine', () => {
+    let vendingMachine, produce, change;
     beforeEach(() => {
         
-        const produce = [
+        produce = [
             {
                 name: 'Orange',
                 price: 1.5,
-                inventory: 6,
-                code: 'A1',
+                inventory: 6
             },
             {
                 name: 'Apple',
                 price: 1.25,
-                inventory: 8,
-                code: 'A2',
+                inventory: 8
             },
             {
                 name: 'Banana',
                 price: 1,
-                inventory: 4,
-                code: 'A3',
+                inventory: 4
             },
             {
                 name: 'Carrot',
                 price: 1.75,
-                inventory: 2,
-                code: 'B1',
+                inventory: 2
             },
             {
                 name: 'Cucumber',
                 price: 1.5,
-                inventory: 0,
-                code: 'B2',
+                inventory: 0
             },
             {
                 name: 'Celery',
                 price: 1,
-                inventory: 10,
-                code: 'B3',
+                inventory: 10
             },
             {
                 name: 'Kiwi',
                 price: 1.35,
-                inventory: 3,
-                code: 'C1',
+                inventory: 3
             },
             {
                 name: 'Mango',
                 price: 2.15,
-                inventory: 9,
-                code: 'C2',
+                inventory: 9
             },
             {
                 name: 'Peach',
                 price: 1.6,
-                inventory: 5,
-                code: 'C3',
+                inventory: 5
             },
             {
                 name: 'Onion',
                 price: 0.8,
-                inventory: 7,
-                code: 'D1',
+                inventory: 7
             },
             {
                 name: 'Zuchini',
                 price: 1.4,
-                inventory: 1,
-                code: 'D2',
+                inventory: 1
             },
             {
                 name: 'Tomato',
                 price: 1.05,
-                inventory: 5,
-                code: 'D3',
+                inventory: 5
             }
-        ]
+        ];
+        change = {
+            0.05: 12,
+            0.1: 10,
+            0.25: 16,
+            1.0: 13,
+            2.0: 3,
+        };
+        vendingMachine = new VendingMachine(produce, change);
+    });
 
-    
-//         // test.data = [test.jermain, test.julianna, test.emmalee];
-    
-//         // test.result = processData(test.data);
-//     });
-
-//     describe('vendingMachine()', () => {
-//         beforeEach(() => {
-//             // test.subject = new studentStats(test.simpleProcessedData)
-//             // test.projectData = test.simpleProcessedData.projects[test.projectName];
-//         });
-//     });
-// });
+    describe('Print vending machine inventory', () => {
+        
+        describe('When you ask for the vending machine inventory', () => {
+            it('will return the array of produce', () => {
+                const result = vendingMachine.printInventory();
+                expect(result).toEqual(
+                    [{name: 'Orange', price: 1.5, inventory: 6},
+                    {name: 'Apple', price: 1.25, inventory: 8},
+                    {name: 'Banana', price: 1, inventory: 4},
+                    {name: 'Carrot', price: 1.75, inventory: 2},
+                    {name: 'Cucumber', price: 1.5, inventory: 0},
+                    {name: 'Celery', price: 1, inventory: 10},
+                    {name: 'Kiwi', price: 1.35, inventory: 3},
+                    {name: 'Mango', price: 2.15,inventory: 9},
+                    {name: 'Peach', price: 1.6, inventory: 5},
+                    {name: 'Onion', price: 0.8, inventory: 7},
+                    {name: 'Zuchini', price: 1.4, inventory: 1},
+                    {name: 'Tomato', price: 1.05, inventory: 5}]
+                );
+            });
+        });
+       
+    });
+});
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,5 +1,12 @@
-class vendingMachine {
+class VendingMachine {
+    constructor(produce, change) {
+        this.produce = [...produce];
+        this.change = {...change};
+    }
 
+    printInventory() {
+        return this.produce  
+    }
 }
 
-module.exports = vendingMachine;
+module.exports = VendingMachine;
